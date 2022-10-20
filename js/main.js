6b73a3de6692f93b9582248df6cf69ecde40234a
swapnil2411 <swapnildarge65@gmail.com>
2022-10-20 20:44:16 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -213,23 +213,7 @@ $(document).ready(function(){
         $('.hamburger').css('visibility','visible')
     })
 
-    WOW.prototype.addBox = function(element) {
-      this.boxes.push(element);
-    };
-  
-    // Init WOW.js and get instance
-    var wow = new WOW();
-    wow.init();
-  
-    // Attach scrollSpy to .wow elements for detect view exit events,
-    // then reset elements and add again for animation
-    $('.wow').on('scrollSpy:exit', function() {
-      $(this).css({
-        'visibility': 'hidden',
-        'animation-name': 'none'
-      }).removeClass('animated');
-      wow.addBox(this);
-    }).scrollSpy();
+    
 
     $('.count-up').countUp({
       'time': 2000,
